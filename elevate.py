2e385118e23a358b92b1a7b77c8332f54ccdc19c
Nicholas Hutchinson <nshutchinson@gmail.com>
2014-02-23 09:23:30 +0000

    It works! Almost.

--- elevate.py
+++ elevate.py
@@ -1,8 +1,22 @@
+"""
+Elevate
+
+Usage:
+  elevate.py [--] <command>...
+
+Options:
+  -h --help     Show this screen.
+
+"""
+
+from docopt import docopt
 import ctypes
 import traceback
 from collections import namedtuple
 import os
 import sys
+import shutil
+# import time
 
 from elevate import win32, libc, utilities
 
@@ -39,6 +53,10 @@ def print_console(*args):
 
 
 def connect_to_parent_console():
+    # FIXME:
+    # - mintty needs special handling (it uses pipes instead of
+    #   bona-finde console handles)
+
     try:
         win32.AttachConsole(win32.ATTACH_PARENT_PROCESS)
     except OSError as e:
@@ -76,41 +94,65 @@ def connect_to_parent_console():
         libc.freopen(handle_info.filename, handle_info.mode,
                      getattr(libc, handle_info.name))
 
+def do_create_process(argv):
+    # FIXME: we need a win32 message pump to avoid the caller getting the 
+    #        wait cursor.
+    # 
+    # class WndProc(utilities.WndProc):
+    #     def wnd_proc(self, window_handle, message_id, w_param, l_param):
+    #         return super().wnd_proc(window_handle, message_id, w_param,
+    #                                 l_param)
+    # window = utilities.create_window(WndProc(), win32.WS_EX_OVERLAPPEDWINDOW,
+    #                                  win32.WS_OVERLAPPEDWINDOW|win32.WS_VISIBLE, "Foobar")
+
+    command_line = utilities.argv_to_command_line(argv)
+    # MSDN says CreateProcess needs a mutable string, for whatever reason.
+    command_line_mutable = ctypes.create_unicode_buffer(command_line)
+
+    # win32.MessageBox(text="{}".format([argv[0], command_line_mutable.value]))
+
+    # FIXME:
+    # - elevated process needs environment of actual caller. CWD too?
+    startup_info = win32.STARTUPINFO()
+    startup_info.cb = ctypes.sizeof(win32.STARTUPINFO)
+
+    proc_info = win32.CreateProcess(argv[0], command_line_mutable,
+        startup_info=startup_info)        
+    win32.WaitForSingleObject(proc_info.process, win32.INFINITE)
+    # FIXME exit code
+    win32.CloseHandle(proc_info.process)
+
 
 def main():
+    # FIXME: docopt not flexible enough, back to argparse we go...
+    params = docopt(__doc__)
+    argv = params['<command>']
 
     if utilities.is_elevated():
-        class WndProc(utilities.WndProc):
-            def wnd_proc(self, window_handle, message_id, w_param, l_param):
-                return super().wnd_proc(window_handle, message_id, w_param,
-                                        l_param)
 
-        window = utilities.create_window(WndProc(), win32.WS_EX_OVERLAPPEDWINDOW,
-                                         win32.WS_OVERLAPPEDWINDOW|win32.WS_VISIBLE, "Foobar")
-        print("before")
-        # windowed subsystem
         connect_to_parent_console()
-        print("printafter\n")
-        write_console("writeconsoleafer\n")
-        libc.printf("printf after\n")
-        utilities.RunLoop().run()
-        assert window
+        do_create_process(argv)
 
     else:
+        resolved_path = shutil.which(argv[0])
+        assert resolved_path
+
         # console subsystem
         exec_info = win32.SHELLEXECUTEINFO()
         exec_info.size = ctypes.sizeof(exec_info)
-        exec_info.file = pythonw_interpreter()
-        exec_info.parameters = utilities.argv_to_command_line([whereami()])
         exec_info.verb = "runas"
         exec_info.mask = (win32.SEE_MASK_FLAG_NO_UI
                           | win32.SEE_MASK_NOASYNC
                           | win32.SEE_MASK_NOCLOSEPROCESS
                           | win32.SEE_MASK_UNICODE)
         exec_info.show = win32.SW_SHOWNORMAL
+        exec_info.file = pythonw_interpreter()
+        exec_info.parameters = utilities.argv_to_command_line(
+            [whereami(), '--', resolved_path] + argv[1:])
 
         win32.ShellExecuteEx(ctypes.byref(exec_info))
         win32.WaitForSingleObject(exec_info.process, win32.INFINITE)
+        # FIXME exit code
         win32.CloseHandle(exec_info.process)
 
 if __name__ == '__main__':
